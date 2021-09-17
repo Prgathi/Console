1baaee25bec50c23cd0d6ad89f88bed8777f81ca
dineshkumar.murugan <dineshkumar.murugan@SYNERGITA154>
2021-09-17 14:28:52 +0530

    Fg

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Log
             Console.WriteLine("Synergita@123");
             Console.WriteLine("SyZSZSfvcxnnergita");
             Console.WriteLine("hfcnhg,mbmfgjm");
+            Console.WriteLine("fdsdfdhgffjj");
 
         }
     }
