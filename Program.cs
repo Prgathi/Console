d8c9cf7cd8e85a319d3b2449706e7e7bee6afc12
dineshkumar.murugan <dineshkumar.murugan@SYNERGITA154>
2021-09-17 14:18:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Synergita@123");
+        }
+    }
+}
